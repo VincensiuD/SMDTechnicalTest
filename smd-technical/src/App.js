d197d9d77b2d34851f2b3e6a9392728c32670b4b
VincensiuD <spanishfrenchromanian@gmail.com>
2022-05-15 21:58:06 +1000

    fix the MainCard instead of hardcoding to mock as it was fetched from API

--- smd-technical/src/App.js
+++ smd-technical/src/App.js
@@ -5,14 +5,33 @@ import { MainCard } from './Components/MainCard';
 import { OverviewCard } from './Components/OverviewCard';
 
 function App() {
+
+let mockAPI=[
+  {icon : "fb", type: "FOLLOWERS", amount: "1987", increment: "12", username:"@nathanf"},
+  {icon : "twt", type: "FOLLOWERS", amount: "1044", increment: "99", username:"@nathanf"},
+  {icon : "insta", type: "FOLLOWERS", amount: "11k", increment: "1099", username:"@realnathanf"},
+  {icon : "yt", type: "SUBSCRIBER", amount: "8239", increment: "-144", username:"Nathan F."},
+];
+
+let mockAPI2 = [
+{title:"Page Views", number:"87", icon:"fb", increment:"3"},
+{title:"Likes", number:"52", icon:"fb", increment:"-2"},
+{title:"Likes", number:"5462", icon:"insta", increment:"2257"},
+        {title:"Profile Views", number:"52k", icon:"insta", increment:"1375"},
+{title:"Retweets", number:"117", icon:"twt", increment:"303"},
+         {title:"Likes", number:"507", icon:"twt", increment:"557"},
+         {title:"Likes", number:"107", icon:"yt", increment:"-19"},
+       {title:"Total Views", number:"1407", icon:"yt", increment:"-12"},
+];
+
+
   return (
     <div className="App">
       <Header totalFollowers="23,004"/>
       <div style={style.mainCard}>
-         <MainCard icon="fb" type="F O L L O W E RS" amount="1987" increment="12" username="@nathanf"/>
-         <MainCard icon="twt" type="F O L L O W E R S" amount="1044" increment="99" username="@nathanf"/>
-         <MainCard icon="insta" type="F O L L O W E R S" amount="11k" increment="1099" username="@realnathanf"/>
-         <MainCard icon="yt" type="S U B S C R I B E R" amount="8239" increment="-144" username="Nathan F."/>
+        {
+        mockAPI.map((x) => <MainCard icon={x.icon} type={x.type} amount={x.amount} increment={x.increment} username={x.username}/>)
+        }  
       </div>      
       <h2 style={{color:"white"}}>Overview - Today</h2>
       <div style={style.mainCard}>
@@ -36,3 +55,9 @@ export default App;
 
  const style = { mainCard: { display: "flex", justifyContent: "space-evenly" }}
 
+
+
+        //   <MainCard icon="fb" type="F O L L O W E R S" amount="1987" increment="12" username="@nathanf"/>
+        //  <MainCard icon="twt" type="F O L L O W E R S" amount="1044" increment="99" username="@nathanf"/>
+        //  <MainCard icon="insta" type="F O L L O W E R S" amount="11k" increment="1099" username="@realnathanf"/>
+        //  <MainCard icon="yt" type="S U B S C R I B E R" amount="8239" increment="-144" username="Nathan F."/> 
--- smd-technical/src/Components/Header.js
+++ smd-technical/src/Components/Header.js
@@ -1,21 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import "./switch.css";
 
 export function Header(props){
 
     const totalFollowers = props.totalFollowers
 
+    const [darkMode,setDarkMode] = useState(true)
+
+    function themeMode(){
+
+        if(darkMode){
+        setDarkMode(false);
+        }
+        else if(!darkMode){
+        setDarkMode(true);
+        }
+    }
+
     return(
         <div style={header}>
             <div>
                 <h1>Social Media Dashboard</h1>
                 <p>Total followers: {totalFollowers}</p>
+                <p>{darkMode.toString()}</p>
             </div>
             <div>
+                <div>                              
                 <input
                 className="react-switch-checkbox"
                 id={`react-switch-new`}
                 type="checkbox"
+                onChange={themeMode}
                 />
                 <label
                 className="react-switch-label"
@@ -23,6 +39,7 @@ export function Header(props){
                 >
                 <span className={`react-switch-button`} />
                 </label>
+                </div>
             </div>
         </div>
       
